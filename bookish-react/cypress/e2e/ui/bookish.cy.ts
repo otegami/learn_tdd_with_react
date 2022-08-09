96c3886a7ff13a8f5b320e5374c294ab5d5550aa
otegami <a.s.takuya1026@gmail.com>
2022-08-10 08:37:39 +0900

    json-server は別アプリとして切り出す

--- bookish-react/cypress/e2e/ui/bookish.cy.ts
+++ bookish-react/cypress/e2e/ui/bookish.cy.ts
@@ -1,4 +1,29 @@
+import axios, { AxiosRequestConfig, AxiosPromise, AxiosResponse } from 'axios';
+
+export const cleanUpStubBooks = () => {
+  return axios.delete('http://localhost:8080/books?_cleanup=true').catch(err => err)
+}
+
+export const feedStubBooks = () => {
+  const books = [
+    { "name": "Refactoring", "id": 1 },
+    { "name": "Domain-driven design", "id": 2 },
+    { "name": "Building Micro-service", "id": 3 }
+  ];
+
+  return books.map(item => axios.post('http://localhost:8080/books', item, { headers: { 'Content-Type': 'application/json' } }).catch(err => err))
+};
+
 describe('Bookish application', () => {
+  beforeEach(() => {
+    feedStubBooks()
+    cy.wait(500)
+  })
+
+  afterEach(() => {
+    cleanUpStubBooks()
+  })
+
   it('Visits the bookish', () => {
     cy.visit('http://localhost:3000/')
     cy.get('h2[data-test="heading"]').contains('Bookish')
@@ -8,10 +33,10 @@ describe('Bookish application', () => {
     cy.visit('http://localhost:3000/')
     cy.get('div[data-test="book-list"]').should('exist')
     cy.get('div.book-item').should((books) => {
-      expect(books).to.have.length(2)
+      expect(books).to.have.length(3)
 
       const titiles = [...books].map(x => x.querySelector('h2').innerHTML)
-      expect(titiles).to.deep.equal(['Refactoring', 'Domain-driven design'])
+      expect(titiles).to.deep.equal(['Refactoring', 'Domain-driven design', 'Building Micro-service'])
     })
   })
 })
