7a0785b1d65b9127d29af554ef3a68ed665aa073
otegami <a.s.takuya1026@gmail.com>
2022-08-11 09:18:33 +0900

    Extranct the logic of getting books from BoolListContainer

---
+++ bookish-react/src/components/BookList/BookList.hooks.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosResponse } from 'axios'
+import { useEffect, useState } from 'react'
+import type { Book } from './BookList'
+
+export const useBookList = (initial: Book[]) => {
+  const [books, setBooks] = useState<Book[]>(initial)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      setError(false)
+      setLoading(true)
+
+      try {
+        const res: AxiosResponse<Book[], unknown> = await axios('http://localhost:8080/books')
+        setBooks(res.data)
+      } catch (e) {
+        setError(true)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchBooks()
+  }, [])
+
+  return { books, loading, error }
+}
