213459f26f20fcc3cd7aee5303bd9f5c94ffd358
otegami <a.s.takuya1026@gmail.com>
2022-08-19 07:55:46 +0900

    Implemented redux book actions

--- bookish-react/src/redux/actions/books.test.ts
+++ bookish-react/src/redux/actions/books.test.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-import { setSearchTerm } from "./books"
+import { FETCH_BOOKS_FAILED, FETCH_BOOKS_PENDING, FETCH_BOOKS_SUCCESS } from '../types/books'
+import { fetchBooks, setSearchTerm } from "./books"
 
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
@@ -24,5 +25,43 @@ describe('BookListContainer related actions', () => {
       { id: 2, name: 'Domain-driven design' }
     ]
     axios.get = jest.fn().mockImplementation(() => Promise.resolve({ data: books }))
+
+    const expectedActions = [
+      { type: FETCH_BOOKS_PENDING },
+      { type: FETCH_BOOKS_SUCCESS, books }
+    ]
+    const store = mockStore({ books: [] })
+
+    return store.dispatch(fetchBooks('')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  })
+
+  it('Fetch data with error', () => {
+    axios.get = jest.fn().mockImplementation(() => Promise.reject({ message: 'Something went wrong' }))
+
+    const expectedActions = [
+      { type: FETCH_BOOKS_PENDING },
+      { type: FETCH_BOOKS_FAILED, err: 'Something went wrong' }
+    ]
+
+    const store = mockStore({ books: [] })
+    return store.dispatch(fetchBooks('')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  })
+
+  it('Search data with term', () => {
+    const books = [
+      { id: 1, name: 'Refactoring' },
+      { id: 2, name: 'Domain-driven design' }
+    ]
+    axios.get = jest.fn().mockImplementation(() => Promise.resolve({ data: books }))
+
+    const store = mockStore({ books: [] })
+
+    return store.dispatch(fetchBooks('domain')).then(() => {
+      expect(axios.get).toHaveBeenCalledWith('http://localhost:8080/books?q=domain')
+    })
   })
 })
--- bookish-react/src/redux/actions/books.ts
+++ bookish-react/src/redux/actions/books.ts
@@ -1,3 +1,19 @@
+import axios from "axios"
+import { FETCH_BOOKS_FAILED, FETCH_BOOKS_PENDING, FETCH_BOOKS_SUCCESS } from "../types/books"
+
 export const setSearchTerm = (term: string) => {
   return { type: 'SET_SEARCH_TERM', term }
+}
+
+export const fetchBooks = (term: string) => {
+  return (dispacth) => {
+    dispacth({ type: FETCH_BOOKS_PENDING })
+    return axios.get(`http://localhost:8080/books?q=${term}`)
+      .then((res) => {
+        dispacth({ type: FETCH_BOOKS_SUCCESS, books: res.data })
+      })
+      .catch((err) => {
+        dispacth({ type: FETCH_BOOKS_FAILED, err: err.message })
+      })
+  }
 }
